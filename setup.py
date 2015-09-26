d154e2646be349a28da86d6f304811f807a32853
IntelliMind <rajanshah@MacBook-Pro.local>
2015-09-26 05:51:08 -0400

    incorporate setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='DX-Analytics',
+		version='1.0',
+		description='DX Analytics',
+		author='Dr. Yves Hilpisch',
+		author_email='dx@tpq.io',
+		url='http://dx-analytics.com/')
