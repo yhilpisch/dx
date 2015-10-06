6b2fe6444877da5196d12ff6237bf41318cc3bec
yves <yves@tpq.io>
2015-10-06 08:41:53 +0200

    Update setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='DX-Analytics',
-		version='1.0',
+		version='0.1',
 		description='DX Analytics',
 		author='Dr. Yves Hilpisch',
 		author_email='dx@tpq.io',
