42943f76a1af3c0e5f08be9e7e2874684c01c118
Toopy <florent@toopy.org>
2017-01-16 20:37:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
+from setuptools import setup, find_packages
 
-from distutils.core import setup
 
-setup(name='DX-Analytics',
-		version='0.1',
-		description='DX Analytics',
-		author='Dr. Yves Hilpisch',
-		author_email='dx@tpq.io',
-		url='http://dx-analytics.com/')
+setup(
+    name='dx',
+    version='0.1',
+    description='DX Analytics',
+    classifiers=[
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Programming Language :: Python',
+    ],
+    author='Dr. Yves Hilpisch',
+    author_email='dx@tpq.io',
+    url='http://dx-analytics.com/',
+    license='AGPLv3+',
+    install_requires=[
+        'pandas-datareader',
+        'scipy',
+        'statsmodels',
+    ],
+    packages=find_packages(),
+)
